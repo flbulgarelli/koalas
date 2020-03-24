b4cc20000049731ce375adedebddd6fbe743caa9
Ana <anavelezrueda@gmail.com>
2020-03-24 12:42:12 -0300

    functions maximum and minimum

--- koalas/__init__.py
+++ koalas/__init__.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
 
-def read_csv(filename, sepator=','):
-    pd.read_csv(filename, sep=sepator)
+def read_csv(filename, sepator):
+    df = pd.read_csv(filename, sep=sepator)
+    return df 
 
 
 def describe(dataframe):
     return dataframe.describe()
+
+def maximum(dataframe):
+    return dataframe.max()
+
+def minimum(dataframe):
+    return dataframe.min()
+
 
 # first(dataframe, count?)
 # last(dataframe, count?)
