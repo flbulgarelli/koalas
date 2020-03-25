103fcc02ac116ece5741a5f42ac1dfb4112bd683
Ana <anavelezrueda@gmail.com>
2020-03-25 15:42:53 -0300

    funtions raising errors

--- koalas/__init__.py
+++ koalas/__init__.py
@@ -1,55 +1,77 @@
+# =======
+# Imports
+# =======
 import pandas as pd
 
+# ============
+# type helpers
+# ============
 
-def read_csv(filename, sepator):
-    df = pd.read_csv(filename, sep=sepator)
-    return df 
+def type_name(value):
+    return type(value).__name__
+
+# =============
+# type checkers
+# =============
+
+def check_single_dataframe(dataframe, function_name): 
+    if type(dataframe) is not pd.DataFrame:
+        raise RuntimeError(f"El argumento de {function_name} debe ser un dataframe, pero vos le pasaste un {type_name(dataframe)}")
+
+def check_single_series(series, function_name): 
+    if type(series) is not pd.Series:
+        raise RuntimeError(f"El argumento de {function_name} debe ser un series, pero vos le pasaste un {type_name(series)}")
+
+def check_single_dataframe_or_series(value, function_name): 
+    if type(value) is not pd.DataFrame and type(value) is not pd.Series:
+        raise RuntimeError(f"El argumento de {function_name} debe ser un dataframe o un series, pero vos le pasaste un {type_name(value)}")
+
+def check_args_count(args, expected_count, message):
+    if len(args) != expected_count: 
+        raise RuntimeError(f"{message}, pero vos le estás pasando {len(args)}")
 
 
-def describe(dataframe):
+# ==============================
+# actual koalas public functions
+# ===============================
+
+
+def read_csv(*args):
+    check_args_count(args, 2, "read_csv tiene que tomar dos argumentos (la ruta y el separador)")
+    filename, separator = args
+
+    if type(filename) is not str:
+        raise RuntimeError(f"El primer argumento de read_csv es una ruta a un archivo y debe ser un string, pero vos le pasaste un {type_name(filename)}")
+
+    if type(separator) is not str:
+        raise RuntimeError(f"El segundo argumento de read_csv es un separador y debe ser un string, pero vos le pasaste un {type_name(separator)}")
+
+    df = pd.read_csv(filename, sep=separator)
+    return df
+
+def describe(*args):
+    check_args_count(args, 1, "describe tiene que tomar un argumento (el dataframe o series)")
+    value, = args
+    check_single_dataframe_or_series(value, 'describe')
     return dataframe.describe()
 
-def maximum(dataframe):
+def maximum(*args):
+    check_args_count(args, 1, "maximum tiene que tomar un argumento (el dataframe o series)")
+    value, = args
+    check_single_dataframe_or_series(value, 'maximum')
     return dataframe.max()
 
-def minimum(dataframe):
+def minimum(*args):
+    check_args_count(args, 1, "minimum tiene que tomar un argumento (el dataframe o series)")
+    value, = args
+    check_single_dataframe_or_series(value, 'minimum')
     return dataframe.min()
 
+def unique(*args):
+    check_args_count(args, 1, "unique tiene que tomar un argumento (el series)")
+    series, = args
+    check_single_series(series, 'unique')
 
-# first(dataframe, count?)
-# last(dataframe, count?)
-# sort_ascending(dataframe, column_name)
-# sort_ascending(series)
-# sort_descending(dataframe, column_name)
-# sort_descending(series)
-
-# bar_plot(series)
-# line_plot(series)
-# scatter_plot(dataframe)
-
-# replace_column(dataframe, column_name, series)
-
-# copy(dataframe)
-# copy(series)
-
-
-# to_duration(dataframe, column_name)
-# to_duration(series)
-
-# to_datetime(dataframe, column_name)
-# to_datetime(series)
-
-# to_float
-# to_int
-
-# drop_nan(dataframe, columns?)
-# fill_nan(dataframe, columns?)
-
-# is_nan
-# is_not_nan
-
-# value_counts(dataframe, column_name)
-# group_by(dataframe, column_name) ????
-
+    return pd.unique(series)
 
 koalas.filter(dataframe, lambda it: it.field  > 0)
