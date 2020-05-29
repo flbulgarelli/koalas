6d6eb847476d51fbd55a8e4e971f3b0ea36567a3
Franco Bulgarelli <franco@mumuki.org>
2020-05-29 10:42:08 -0300

    Format

--- koalas/__init__.py
+++ koalas/__init__.py
@@ -2,33 +2,36 @@
 # Imports
 # =======
 import pandas as pd
+import geopy
+import folium
+from folium import plugins
 
 # ============
 # type helpers
 # ============
 
 def type_name(value):
-    return type(value).__name__
+  return type(value).__name__
 
 # =============
 # type checkers
 # =============
 
 def check_single_dataframe(dataframe, function_name):
-    if type(dataframe) is not pd.DataFrame:
-        raise RuntimeError(f"El argumento de {function_name} debe ser un dataframe, pero vos le pasaste un {type_name(dataframe)}")
+  if type(dataframe) is not pd.DataFrame:
+    raise RuntimeError(f"El argumento de {function_name} debe ser un dataframe, pero vos le pasaste un {type_name(dataframe)}")
 
 def check_single_series(series, function_name):
-    if type(series) is not pd.Series:
-        raise RuntimeError(f"El argumento de {function_name} debe ser un series, pero vos le pasaste un {type_name(series)}")
+  if type(series) is not pd.Series:
+    raise RuntimeError(f"El argumento de {function_name} debe ser un series, pero vos le pasaste un {type_name(series)}")
 
 def check_single_dataframe_or_series(value, function_name):
-    if type(value) is not pd.DataFrame and type(value) is not pd.Series:
-        raise RuntimeError(f"El argumento de {function_name} debe ser un dataframe o un series, pero vos le pasaste un {type_name(value)}")
+  if type(value) is not pd.DataFrame and type(value) is not pd.Series:
+    raise RuntimeError(f"El argumento de {function_name} debe ser un dataframe o un series, pero vos le pasaste un {type_name(value)}")
 
 def check_args_count(args, expected_count, message):
-    if len(args) != expected_count:
-        raise RuntimeError(f"{message}, pero vos le estás pasando {len(args)}")
+  if len(args) != expected_count:
+    raise RuntimeError(f"{message}, pero vos le estás pasando {len(args)}")
 
 
 # ==============================
@@ -37,39 +40,40 @@ def check_args_count(args, expected_count, message):
 
 
 def read_csv(*args):
-    check_args_count(args, 2, "read_csv tiene que tomar dos argumentos (la ruta y el separador)")
-    filename, separator = args
+  check_args_count(args, 2, "read_csv tiene que tomar dos argumentos (la ruta y el separador)")
+  filename, separator = args
 
-    if type(filename) is not str:
-        raise RuntimeError(f"El primer argumento de read_csv es una ruta a un archivo y debe ser un string, pero vos le pasaste un {type_name(filename)}")
+  if type(filename) is not str:
+    raise RuntimeError(f"El primer argumento de read_csv es una ruta a un archivo y debe ser un string, pero vos le pasaste un {type_name(filename)}")
 
-    if type(separator) is not str:
-        raise RuntimeError(f"El segundo argumento de read_csv es un separador y debe ser un string, pero vos le pasaste un {type_name(separator)}")
+  if type(separator) is not str:
+    raise RuntimeError(f"El segundo argumento de read_csv es un separador y debe ser un string, pero vos le pasaste un {type_name(separator)}")
 
-    df = pd.read_csv(filename, sep=separator)
-    return df
+  df = pd.read_csv(filename, sep=separator)
+  return df
 
 def describe(*args):
-    check_args_count(args, 1, "describe tiene que tomar un argumento (el dataframe o series)")
-    value, = args
-    check_single_dataframe_or_series(value, 'describe')
-    return dataframe.describe()
+  check_args_count(args, 1, "describe tiene que tomar un argumento (el dataframe o series)")
+  value, = args
+  check_single_dataframe_or_series(value, 'describe')
+  return dataframe.describe()
 
 def maximum(*args):
-    check_args_count(args, 1, "maximum tiene que tomar un argumento (el dataframe o series)")
-    value, = args
-    check_single_dataframe_or_series(value, 'maximum')
-    return dataframe.max()
+  check_args_count(args, 1, "maximum tiene que tomar un argumento (el dataframe o series)")
+  value, = args
+  check_single_dataframe_or_series(value, 'maximum')
+  return dataframe.max()
 
 def minimum(*args):
-    check_args_count(args, 1, "minimum tiene que tomar un argumento (el dataframe o series)")
-    value, = args
-    check_single_dataframe_or_series(value, 'minimum')
-    return dataframe.min()
+  check_args_count(args, 1, "minimum tiene que tomar un argumento (el dataframe o series)")
+  value, = args
+  check_single_dataframe_or_series(value, 'minimum')
+  return dataframe.min()
 
 def unique(*args):
-    check_args_count(args, 1, "unique tiene que tomar un argumento (el series)")
-    series, = args
-    check_single_series(series, 'unique')
+  check_args_count(args, 1, "unique tiene que tomar un argumento (el series)")
+  series, = args
+  check_single_series(series, 'unique')
 
-    return pd.unique(series)
+  return pd.unique(series)
+
